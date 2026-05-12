c963e672493887f5288a2ec22c187286ff62ff50
Jess Price <jessprice144@gmail.com>
2026-05-12 11:24:52 +0100

    feat: brainstorm latent space plotting of ae & vae

---
+++ notebooks/04_latent_space_analysis.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: mnist-autoencoders
+#     language: python
+#     name: mnist-autoencoders
+# ---
+
+# %%
+# TODO:
+# Recap principal component analysis:
+# -1st principle component minimises mean squared distance to the line, next components are just orthogonal(?)
+# Plan architecture to display latent space of ae & vae
+# Conduct test, using appropriate visualisations
