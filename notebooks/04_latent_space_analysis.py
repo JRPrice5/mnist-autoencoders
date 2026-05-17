1b5aa58670a35ec231aefcb8e12d569c893b7b29
Jess Price <jessprice144@gmail.com>
2026-05-17 11:13:45 +0100

    feat: create latent space todos

--- notebooks/04_latent_space_analysis.py
+++ notebooks/04_latent_space_analysis.py
@@ -8,9 +8,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.19.1
 #   kernelspec:
-#     display_name: mnist-autoencoders
+#     display_name: Python 3 (ipykernel)
 #     language: python
-#     name: mnist-autoencoders
+#     name: python3
 # ---
 
 # %%
@@ -19,3 +19,9 @@
 # -1st principle component minimises mean squared distance to the line, next components are just orthogonal(?)
 # Plan architecture to display latent space of ae & vae
 # Conduct test, using appropriate visualisations
+
+# Motives for pca'ing ae and vae:
+# getting a view of mnist specific latent space
+# bettering my understanding of pca
+# better my understanding of ae and vae
+# pca, ae, vae directly inform my course prep for ml modules too
